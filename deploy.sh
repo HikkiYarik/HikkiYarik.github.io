a4b895115e699490c75f62ea405637b926729c00
Ярослав <wowkulaka555@gmail.com>
2022-02-01 12:35:01 +0200

    changes in vue.config.js v9

--- deploy.sh
+++ deploy.sh
@@ -17,8 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # если вы публикуете по адресу https://<USERNAME>.github.io
-# -
-# d
+# git push -f https://github.com/HikkiYarik/HikkiYarik.github.io.git master
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
