c7b0f1c2c51939c8ab669e9e81f25a8ea644f065
Ярослав <wowkulaka555@gmail.com>
2022-02-03 15:24:14 +0200

    changes in deploy v1

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # если вы публикуете по адресу https://<USERNAME>.github.io
-# git push -f https://github.com/HikkiYarik/HikkiYarik.github.io.git master
+git push -f https://github.com/HikkiYarik/HikkiYarik.github.io.git gh-pages
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
